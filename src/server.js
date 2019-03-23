d55ca204bc90302306633752d8dbcc180f920742
abiassantana <abias.santana@dce.ufpb.br>
2019-03-23 15:15:47 -0300

    Server initial configurations

---
+++ src/server.js
@@ -0,0 +1,11 @@
+let express = require('express');
+let app = express();
+//Data base
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000 pf!');
+});
