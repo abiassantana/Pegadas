94b5dc5aa158bf5e080e9981d7fb22ac781566d9
Marcelo S Santos <marcelo.souza@dce.ufpb.br>
2019-03-30 14:20:44 -0300

    Telas (login_dev, cadastro_dev) e organização das pastas do projeto

--- src/server.js
+++ src/server.js
@@ -4,8 +4,10 @@ let app = express();
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // for parsing application/x-www-form-urlencoded
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000 pf!');
+    console.log('Example app listening on port 3000 pf!');
 });
