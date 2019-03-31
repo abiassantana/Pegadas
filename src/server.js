9d4bc555ad840119455626c84d7a6dec1b3a9094
Marcelo S Santos <marcelo.souza@dce.ufpb.br>
2019-03-31 16:43:51 -0300

    Telas (login_dev, cadastro_dev) e organização das pastas do projeto

--- src/server.js
+++ src/server.js
@@ -6,17 +6,19 @@ const db = require('./models/index');
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // for parsing application/x-www-form-urlencoded
 require('./routes/developerUrls')(app);
 db.sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000 pf!');
+    console.log('Example app listening on port 3000 pf!');
 });
